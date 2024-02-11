2bfa0a0dc43cc2ea369ab00290e7467b95c89663
PavelE13 <94640966+PavelE13@users.noreply.github.com>
2024-02-11 19:22:16 +0300

    Add files via upload

---
+++ HW3/Lect3/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  HW2
+//
+//  Created by user on 04.02.2024.
+//  Copyright © 2024 user. All rights reserved.
+//
+
+import UIKit
+
+class Constants: UICollectionViewCell {
+    
+        struct Identifier {
+            static let photoCellIdentifier = "PhotoCell" // Идентификатор вашей ячейки
+        }
+    }
